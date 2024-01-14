d6c2c33177e8799aeb789c55bd5b0b20799f37b4
AntoBrandi <antonio.brandi@outlook.it>
2024-01-14 12:25:09 +0100

    added code for Section3

--- Section3_Global_Localization/bumperbot_ws/src/bumperbot_description/launch/gazebo.launch.py
+++ Section3_Global_Localization/bumperbot_ws/src/bumperbot_description/launch/gazebo.launch.py
@@ -1,9 +1,10 @@
 import os
+from os import environ, pathsep
 from ament_index_python.packages import get_package_share_directory, get_package_prefix
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, PythonExpression
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -12,16 +13,29 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
     bumperbot_description_dir = get_package_share_directory("bumperbot_description")
-    bumperbot_description_share = os.path.join(get_package_prefix("bumperbot_description"), "share")
     gazebo_ros_dir = get_package_share_directory("gazebo_ros")
 
     model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
-                                        bumperbot_description_dir, "urdf", "bumperbot.urdf.xacro"
-                                        ),
+                                      bumperbot_description_dir, "urdf", "bumperbot.urdf.xacro"
+                                      ),
                                       description="Absolute path to robot urdf file"
     )
+    
+    world_name_arg = DeclareLaunchArgument(name="world_name", default_value="empty")
 
-    env_var = SetEnvironmentVariable("GAZEBO_MODEL_PATH", bumperbot_description_share)
+    world_path = PathJoinSubstitution([
+            bumperbot_description_dir,
+            "worlds",
+            PythonExpression(expression=["'", LaunchConfiguration("world_name"), "'", " + '.world'"])
+        ]
+    )
+    model_path = os.path.join(get_package_prefix("bumperbot_description"), "share")
+    model_path += pathsep + os.path.join(get_package_share_directory("bumperbot_description"), "models")
+    
+    if 'GAZEBO_MODEL_PATH' in environ:
+        model_path += pathsep + environ['GAZEBO_MODEL_PATH']
+
+    env_var = SetEnvironmentVariable("GAZEBO_MODEL_PATH", model_path)
 
     robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
                                        value_type=str)
@@ -29,13 +43,17 @@ def generate_launch_description():
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[{"robot_description": robot_description}]
+        parameters=[{"robot_description": robot_description,
+                     "use_sim_time": True}]
     )
 
     start_gazebo_server = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(gazebo_ros_dir, "launch", "gzserver.launch.py")
-        )
+        ),
+        launch_arguments={
+            "world": world_path,
+        }.items(),
     )
 
     start_gazebo_client = IncludeLaunchDescription(
@@ -54,6 +72,7 @@ def generate_launch_description():
     return LaunchDescription([
         env_var,
         model_arg,
+        world_name_arg,
         start_gazebo_server,
         start_gazebo_client,
         robot_state_publisher_node,
