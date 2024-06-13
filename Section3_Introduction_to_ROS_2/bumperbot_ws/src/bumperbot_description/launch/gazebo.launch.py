c8f7c25c436801a1d2c6498dcb486224b87c850a
AntoBrandi <antonio.brandi@outlook.it>
2024-06-13 21:28:31 +0200

    update section names

---
+++ Section3_Introduction_to_ROS_2/bumperbot_ws/src/bumperbot_description/launch/gazebo.launch.py
@@ -0,0 +1,65 @@
+import os
+from os import pathsep
+from ament_index_python.packages import get_package_share_directory, get_package_prefix
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    bumperbot_description = get_package_share_directory("bumperbot_description")
+    bumperbot_description_prefix = get_package_prefix("bumperbot_description")
+    gazebo_ros_dir = get_package_share_directory("gazebo_ros")
+
+    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
+                                        bumperbot_description, "urdf", "bumperbot.urdf.xacro"
+                                        ),
+                                      description="Absolute path to robot urdf file"
+    )
+
+    model_path = os.path.join(bumperbot_description, "models")
+    model_path += pathsep + os.path.join(bumperbot_description_prefix, "share")
+
+    env_var = SetEnvironmentVariable("GAZEBO_MODEL_PATH", model_path)
+
+    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
+                                       value_type=str)
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description}]
+    )
+
+    start_gazebo_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(gazebo_ros_dir, "launch", "gzserver.launch.py")
+        )
+    )
+
+    start_gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(gazebo_ros_dir, "launch", "gzclient.launch.py")
+        )
+    )
+
+    spawn_robot = Node(package="gazebo_ros", executable="spawn_entity.py",
+                        arguments=["-entity", "bumperbot",
+                                   "-topic", "robot_description",
+                                  ],
+                        output="screen"
+    )
+
+    return LaunchDescription([
+        env_var,
+        model_arg,
+        start_gazebo_server,
+        start_gazebo_client,
+        robot_state_publisher_node,
+        spawn_robot
+    ])
