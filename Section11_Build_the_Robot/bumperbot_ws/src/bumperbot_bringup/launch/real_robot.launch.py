0a3ed06e708adb19cbcf5b25ed7d7cb7ed5e2936
AntoBrandi <antonio.brandi@outlook.it>
2024-07-05 18:40:23 +0200

    start real robot functionalities

--- Section11_Build_the_Robot/bumperbot_ws/src/bumperbot_bringup/launch/real_robot.launch.py
+++ Section11_Build_the_Robot/bumperbot_ws/src/bumperbot_bringup/launch/real_robot.launch.py
@@ -1,16 +1,38 @@
 import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+    use_slam = LaunchConfiguration("use_slam")
+
+    use_slam_arg = DeclareLaunchArgument(
+        "use_slam",
+        default_value="false"
+    )
+
     hardware_interface = IncludeLaunchDescription(
         os.path.join(
             get_package_share_directory("bumperbot_firmware"),
             "launch",
             "hardware_interface.launch.py"
         ),
+    )
+
+    laser_driver = Node(
+            package="rplidar_ros",
+            executable="rplidar_node",
+            name="rplidar_node",
+            parameters=[os.path.join(
+                get_package_share_directory("bumperbot_bringup"),
+                "config",
+                "rplidar_a1.yaml"
+            )],
+            output="screen"
     )
     
     controller = IncludeLaunchDescription(
@@ -32,9 +54,38 @@ def generate_launch_description():
             "joystick_teleop.launch.py"
         ),
     )
+
+    safety_stop = Node(
+        package="bumperbot_utils",
+        executable="safety_stop",
+        output="screen",
+    )
+
+    localization = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("bumperbot_localization"),
+            "launch",
+            "global_localization.launch.py"
+        ),
+        condition=UnlessCondition(use_slam)
+    )
+
+    slam = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("bumperbot_mapping"),
+            "launch",
+            "slam.launch.py"
+        ),
+        condition=IfCondition(use_slam)
+    )
     
     return LaunchDescription([
+        use_slam_arg,
         hardware_interface,
+        laser_driver,
         controller,
         joystick,
+        safety_stop,
+        localization,
+        slam
     ])
