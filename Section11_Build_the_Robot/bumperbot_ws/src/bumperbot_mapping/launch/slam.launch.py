98bfa0990171658d5eb7bd68c7f47d92d7f6c4c0
AntoBrandi <antonio.brandi@outlook.it>
2025-03-07 17:06:01 +0100

    add slam_toolbox to managed nodes

--- Section11_Build_the_Robot/bumperbot_ws/src/bumperbot_mapping/launch/slam.launch.py
+++ Section11_Build_the_Robot/bumperbot_ws/src/bumperbot_mapping/launch/slam.launch.py
@@ -3,14 +3,17 @@ from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
 
     use_sim_time = LaunchConfiguration("use_sim_time")
     slam_config = LaunchConfiguration("slam_config")
+
+    ros_distro = os.environ["ROS_DISTRO"]
     lifecycle_nodes = ["map_saver_server"]
+    lifecycle_nodes.extend(["slam_toolbox"] if ros_distro == "humble" else [])
 
     use_sim_time_arg = DeclareLaunchArgument(
         "use_sim_time",
